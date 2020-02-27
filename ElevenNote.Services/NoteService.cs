010adc3c0e98c9d05f4b89b3ba6ba2e0fa6690f5
Marshall Davis <marmadav@gmail.com>
2020-02-27 14:35:32 -0500

    sure

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
 
 namespace ElevenNote.Services
 {
@@ -76,6 +78,34 @@ namespace ElevenNote.Services
             }//*Hell yeah
             //*iytlihf
         }
+        public bool UpdateNote(NoteEdit model)
+        {
+            using(var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.NoteId == model.NoteId && e.OwnerId == _userId);
+                entity.Title = model.Title;
+                entity.Content = model.Content;
+                entity.ModifiedUtc = DateTimeOffset.UtcNow;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        /*public IHttpActionResult Put(NoteEdit note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNotService();
+
+            if (!service.UpdateNote(note))
+                return InternalServerError();
+
+            return Ok();
+        }*/
 
     }
 }
